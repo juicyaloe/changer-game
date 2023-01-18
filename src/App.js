faee7c8e4f0bc98b4456d197d23511cbec90343c
Min-jae Kim <mink4661@snu.ac.kr>
2023-01-19 00:27:19 +0900

    메인 화면 일부 구현

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import Main from './pages/main';
+import Search from './pages/search';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <CssBaseline />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/search" element={<Search />} />
       </Routes>
     </Fragment>
   );
---
+++ src/pages-components/main/mainImage.tsx
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+
+const MainImage = styled.img`
+  width: 100%;
+  background-size: cover;
+`;
+
+export default MainImage;
